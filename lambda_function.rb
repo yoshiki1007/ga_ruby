55adaadc372a5169409ba32a718ea8108de7878b
yoshiki_murakami <eiim.yoshiki@gmail.com>
2022-01-28 00:02:57 +0900

    add: メインファイルの追加

---
+++ lambda_function.rb
@@ -0,0 +1,23 @@
+require 'dotenv/load'
+require 'google/apis/analytics_v3'
+
+client = Google::Apis::AnalyticsV3::AnalyticsService.new
+client.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+  json_key_io: File.open('./key_file.json'),
+  scope: ['https://www.googleapis.com/auth/analytics.readonly']
+)
+
+data = client.get_ga_data(
+  "ga:#{ENV['VIEW_ID']}",
+  (Date.today - 7).strftime,
+  Date.today.strftime,
+  "ga:pageviews",
+  {
+    dimensions: 'ga:date',
+    sort: "-ga:date"
+  }
+)
+
+data.rows.each do |row|
+  p row
+end
