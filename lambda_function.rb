131c38ee5c4bb094f5761b88449bfb6a7fa4c850
yoshiki_murakami <eiim.yoshiki@gmail.com>
2022-01-28 00:38:16 +0900

    ref: scopeをenvへ修正

--- lambda_function.rb
+++ lambda_function.rb
@@ -4,7 +4,7 @@ require 'google/apis/analytics_v3'
 client = Google::Apis::AnalyticsV3::AnalyticsService.new
 client.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
   json_key_io: File.open('./key_file.json'),
-  scope: ['https://www.googleapis.com/auth/analytics.readonly']
+  scope: ENV['SCOPE']
 )
 
 data = client.get_ga_data(
